703adbecbe5a478a4ed954b9c17cc8d9434e58c6
arizonseed <749817547@qq.com>
2018-03-11 10:00:13 +0800

    proxy conf

--- src/app/draw-setting/complete-pump-set/complete-pump-set.component.ts
+++ src/app/draw-setting/complete-pump-set/complete-pump-set.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./complete-pump-set.component.css']
 })
 export class CompletePumpSetComponent implements OnInit {
+  completePumpSet:CompeletePumpSet;
 
   constructor() { }
 
   ngOnInit() {
+    this.completePumpSet = new CompeletePumpSet(
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      'cw',
+    );
   }
 
+}
+
+export class CompeletePumpSet{
+  constructor(
+    public frequency:number,
+    public _type:number,
+    public flangRating:number,
+    public motorBrand:number,
+    public poles:number,
+    public moterPower:number,
+    public voltage:number,
+    public coupling:number,
+    public rotaion:string
+  ){}
 }
